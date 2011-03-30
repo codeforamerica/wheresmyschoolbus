92aaee5bdcd59b6a1c90b2e446187fa5ec3ea3e4
Joel Mahoney <joelmahoney@gmail.com>
2011-03-30 16:39:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheresmybus::Application.config.secret_token = 'a94bc9c8d187e6dc704e83f371026446dabed2cf51093a82331e44c616ffaaf0dd981dac086903e206c59ab33b93132093ec0fcc34f40a9bad8c8d3fd1e0f1c7'

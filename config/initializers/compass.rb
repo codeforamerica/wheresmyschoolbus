f24d43af298601c0c2af41a4a920f5b390571c3d
Joel Mahoney <joelmahoney@gmail.com>
2011-04-22 11:19:26 -0700

    make heroku play nice with compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,14 @@
 require 'compass'
 require 'compass/app_integration/rails'
+require 'fileutils'
+
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
 Compass::AppIntegration::Rails.initialize!
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")

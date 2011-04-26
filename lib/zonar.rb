c18dc96898f359b02dc3308b6a38b42090e3bee4
T S <YenTheFirst@gmail.com>
2011-04-26 16:39:15 -0700

    add some documentation thingies

--- lib/zonar.rb
+++ lib/zonar.rb
@@ -7,11 +7,13 @@ module Zonar
       @api = "http://#{subdomain}.zonarsystems.net/interface.php"
     end
 
+	#gets all fleet_ids in the fleet.
     def fleet
       params = {:username => @username, :password => @password, :action => 'showopen', :operation => 'showassets', :format => 'json'}
       HTTParty.get(@api, {:query => params, :headers => {"User-Agent" => "User-Agent: curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3"}}).parsed_response
     end
 
+	#gets Data of single bus
     def bus(fleet_id)
       params = {:username => @username, :password => @password, :action => 'showposition', :type => "Standard", :logvers => "3", :operation => 'current', :format => 'xml', :reqtype => "fleet", :target => fleet_id.gsub(' ', '%20')}
       HTTParty.get(@api, {:query => params, :headers => {"User-Agent" => "User-Agent: curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3"}}).parsed_response

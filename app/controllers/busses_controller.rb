97f9e20d07e40ccc2c6f095fc00b8f7d29f99c43
maxogden <max@maxogden.com>
2011-04-27 14:40:58 -0700

    refactor bus lookup code and add a json route for fetching a single bus location

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -4,20 +4,44 @@ class BussesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @busses = current_user.busses.map do |b| 
-      location = $zonar.bus(b.fleet_id)
-      next unless location.keys.include? 'currentlocations'
-      { 
-        :type => "Point",
-        :coordinates => [
-          location['currentlocations']['asset']['long'],
-          location['currentlocations']['asset']['lat']
-        ]
-      }
-    end.compact
+    if params[:fleet_id]
+      if bus = Bus.find_by_fleet_id(params[:fleet_id])
+        @busses = fetch_bus_locations([bus])
+      else
+        @busses = []
+      end
+      render :json => @busses.to_json
+    else
+      @busses = fetch_bus_locations(current_user.busses)
+    end
+  end
+  
+  def show
   end
   
   private
+  def fetch_bus_locations(busses)
+    busses.map do |b|
+      location = $zonar.bus(b.fleet_id)
+      next unless location.keys.include? 'currentlocations'
+      bus_geojson_from(location)
+    end.compact
+  end
+  
+  def bus_geojson_from(location)
+    {
+      :type => "Feature",
+      :geometry => {
+        :type => "Point",
+        :coordinates => [
+          location['currentlocations']['asset'].delete('long'),
+          location['currentlocations']['asset'].delete('lat')
+        ]
+      },
+      :properties => location['currentlocations']['asset']
+    }
+  end
+  
   def auth_user_or_admin
     #TODO: write
   end

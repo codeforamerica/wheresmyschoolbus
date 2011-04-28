90db92610d48377b2c49acff876b51826339b941
T S <YenTheFirst@gmail.com>
2011-04-28 13:56:32 -0700

    added name to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110426233429) do
+ActiveRecord::Schema.define(:version => 20110428202239) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(:version => 20110426233429) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

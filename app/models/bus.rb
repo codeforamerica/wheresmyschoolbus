821ada1467a15152e2cb6e3e8640e58a5d814b2e
Scott Silverman <scottsil@me.com>
2011-06-20 15:29:01 -0700

    bus displays nickname and fleet_id

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -2,6 +2,6 @@ class Bus < ActiveRecord::Base
   belongs_to :user
   
   def nickname
-    self['nickname'].blank? ? self.fleet_id : self['nickname']
+    self['nickname'].blank? ? self.fleet_id : "#{self['nickname']} - #{self.fleet_id}"
   end
 end

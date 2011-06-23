0a5b6b952506aa96af6793ffc082bdea46634463
T S <YenTheFirst@gmail.com>
2011-06-23 11:54:03 -0700

    mantain flash state through the redirector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def index
     if admin_signed_in?
-      redirect_to :users
+      redirect_to :users, :flash=>flash
     elsif user_signed_in?
-      redirect_to :busses
+      redirect_to :busses, :flash=>flash
     else
-      redirect_to "/welcome"
+      redirect_to "/welcome", :flash=>flash
     end
     #render login page by default
   end
